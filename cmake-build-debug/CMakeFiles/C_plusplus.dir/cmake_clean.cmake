bcc81bf4e201f6d754eb5bed4092e5e9defb0e9c
OlegElizarov <elizarov1999@list.ru>
2019-09-29 17:46:12 +0300

    create

---
+++ cmake-build-debug/CMakeFiles/C_plusplus.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_plusplus.dir/2_task.cpp.o"
+  "C_plusplus"
+  "C_plusplus.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C_plusplus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
