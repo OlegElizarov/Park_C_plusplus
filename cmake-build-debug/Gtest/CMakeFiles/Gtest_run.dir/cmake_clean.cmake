81a0cdb07f8bf961289c41c1c311e030f754159a
Oleg <elizarov1999@list.ru>
2019-10-09 10:49:14 +0300

    new cmake for travis

---
+++ cmake-build-debug/Gtest/CMakeFiles/Gtest_run.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Gtest_run.dir/test.o"
+  "Gtest_run"
+  "Gtest_run.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Gtest_run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
