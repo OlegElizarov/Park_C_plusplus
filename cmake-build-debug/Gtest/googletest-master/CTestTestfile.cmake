75fad67d53c3ccf8660933539903caa9c50b69d2
Oleg <elizarov1999@list.ru>
2019-10-10 18:38:03 +0300

    del

--- cmake-build-debug/Gtest/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/elenaelizarova/CLionProjects/Park_C_plusplus/Gtest/googletest-master
-# Build directory: /Users/elenaelizarova/CLionProjects/Park_C_plusplus/cmake-build-debug/Gtest/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
