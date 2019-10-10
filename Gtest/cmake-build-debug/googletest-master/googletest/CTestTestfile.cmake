6be8cb0e663979bd65ced3c5631b84be45e7af79
Oleg <elizarov1999@list.ru>
2019-10-10 18:27:47 +0300

    delete

--- Gtest/cmake-build-debug/googletest-master/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/elenaelizarova/CLionProjects/Park_C_plusplus/Gtest/googletest-master/googletest
-# Build directory: /Users/elenaelizarova/CLionProjects/Park_C_plusplus/Gtest/cmake-build-debug/googletest-master/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
---
+++ cmake-build-debug/Gtest/CMakeFiles/Gtest_runn.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/elenaelizarova/CLionProjects/Park_C_plusplus/Gtest/test.cpp" "/Users/elenaelizarova/CLionProjects/Park_C_plusplus/cmake-build-debug/Gtest/CMakeFiles/Gtest_runn.dir/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Gtest/googletest-master/googletest/src"
+  "../Gtest/C_plusplus"
+  "../Gtest/googletest-master/googletest/include"
+  "../Gtest/googletest-master/googletest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/elenaelizarova/CLionProjects/Park_C_plusplus/cmake-build-debug/Gtest/googletest-master/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  "/Users/elenaelizarova/CLionProjects/Park_C_plusplus/cmake-build-debug/Gtest/googletest-master/googletest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
