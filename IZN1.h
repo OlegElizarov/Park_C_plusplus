be776ffc7e3e0023a24d21448d8354d3c4da5c3e
Oleg <43301170+OlegElizarov@users.noreply.github.com>
2019-10-11 19:03:25 +0300

    Update and rename IZN1.h to func.h

--- IZN1.h
+++
@@ -1,7 +0,0 @@
-#ifndef GTEST_RUN_IZN1_H
-#define GTEST_RUN_IZN1_H
-void delete_column(int *matr[],int row,int *col,int delcol);
-
-void delete_row(int *matr[],int delrow,int *row);
-
-#endif //GTEST_RUN_IZN1_H
---
+++ func.h
@@ -0,0 +1,6 @@
+#ifndef GTEST_RUN_FUNC_H
+#define GTEST_RUN_FUNC_H
+
+void zero_dawn(int *matr[], int *rows, int *columns, int *(*res[]));
+
+#endif //GTEST_RUN_FUNC_H
