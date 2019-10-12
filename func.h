106fcbf23d5b9f9c45e8fc8e3b7cc8b155597398
Oleg <43301170+OlegElizarov@users.noreply.github.com>
2019-10-12 19:14:19 +0300

    Update func.h

--- func.h
+++ func.h
@@ -2,5 +2,6 @@
 #define GTEST_RUN_FUNC_H
 
 void zero_dawn(int *matr[], int *rows, int *columns, int *(*res[]));
-
+void delete_column(int *matr[], int row, int *col, int delcol);
+void delete_row(int *matr[], int delrow, int *row);
 #endif //GTEST_RUN_FUNC_H
