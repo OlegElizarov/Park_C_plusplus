05c336b04354e8ac880d3332632dfc03ed8a6ec5
Oleg <43301170+OlegElizarov@users.noreply.github.com>
2019-10-26 14:57:07 +0300

    Update func2lin.c

--- func2lin.c
+++ func2lin.c
@@ -11,17 +11,6 @@ pthread_mutex_t lock;
 
 enum { BUF_LENGTH = 100*1024*1024 };
 
-typedef struct Args_tag {
-    int i;
-    int end;
-    int *res;
-    char* buf;
-
-} someArgs_t;
-
-atomic_long  fsize=0;
-bool costyl=false;
-atomic_int lastcount=0;
 
 void* lin_search(char* buf,int begin,int end,int *res)
 {
